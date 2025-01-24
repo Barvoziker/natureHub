63686a41ca380b8f55008ea9a3de04f82d8ebe57
Mathis Buchet <58893042+Barvoziker@users.noreply.github.com>
2025-01-24 11:48:30 +0100

    initial comit + lealfet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map: L.Map | undefined; // Initialisation explicite avec `undefined`
+  centerMarker: L.Marker | undefined; // Idem pour `centerMarker`
 
+  ngOnInit() {
+    this.initMap();
+  }
+
+  initMap() {
+    this.map = L.map('map').setView([48.866667, 2.333333], 12); // Initialisation de `map`
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors',
+    }).addTo(this.map);
+
+    // Forcer le recalcul de la taille de la carte après un court délai
+    setTimeout(() => {
+      if (this.map) {
+        this.map.invalidateSize();
+      }
+    }, 100); // Délai de 100 ms
+  }
+
+  addPointAtCenter() {
+    if (!this.map) return; // Vérification que `map` est bien initialisé
+
+    const center = this.map.getCenter();
+
+    if (this.centerMarker) {
+      this.map.removeLayer(this.centerMarker);
+    }
+
+    this.centerMarker = L.marker([center.lat, center.lng], {
+      icon: L.icon({
+        iconUrl: 'assets/icon/marker-icon.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+      }),
+    }).addTo(this.map);
+
+    this.centerMarker.bindPopup('Nouveau point ajouté !').openPopup();
+  }
 }
